582cc487736b47b2f5b9a70d66864ffae1c5ed26
root <root@localhost.localdomain>
2014-11-17 12:05:47 +0000

    Mon Nov 17 12:05:47 UTC 2014

--- solutions/282/euler282.c
+++ solutions/282/euler282.c
@@ -1,42 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-using namespace std;
-
 long int		mod = 1475789056;
 
 long int		ack(int val, int mod)
 {
-	long int	m = 1, pom, ret, i;
+	long int	i, nMod, ret, tmp = 1;
 	long int	*niz = (long int *)malloc(sizeof(long int) * 25000000);
 
 	if (val == 1)
 		return (2);
-	if (mod == 2)
-		return (0);
 	for (i=1; i <= 25000000; i++)
 	{
-		m = (m * 2) % mod;
-		if (m < 25000000 && niz[m])
+		if ((tmp = (tmp * 2) % mod) < 25000000 && niz[tmp])
 		{
-			pom = i - niz[m];
+			nMod = i - niz[tmp];
 			break;
 		}
-		if (m < 25000000)
-			niz[m] = i;
+		if (tmp < 25000000)
+			niz[tmp] = i;
 	}
-	ret = ack(val - 1, pom);
-	for (i=1, m=1; i <= ret; i++)
-		m = (m * 2) % mod;
-	return (m);
+	ret = ack(val - 1, nMod);
+	for (i=1, tmp=1; i <= ret; i++)
+		tmp = (tmp * 2) % mod;
+	return (tmp);
 }
 
 int				main()
 {
-	int			prevack = 1 + 3 + 7 + 61;
-	long int	res = (ack(7, mod) + 2 * ack(10, mod) - (3 * 3) + prevack) % mod;
+	int			srcAck = 1 + 3 + 7 + 61;
 
-	printf("%ld\n", res);
+	printf("%ld\n", (ack(7, mod) + 2 * ack(10, mod) - (3 * 3) + srcAck) % mod);
 	return 0;
 }
 
